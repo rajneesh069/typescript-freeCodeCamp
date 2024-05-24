2bf88c6689fd258cbc4984fc16e9e7d8ddfa0362
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-05-24 23:38:26 +0530

    complete union types and variables in TS

---
+++ unionTypesInTS/unions.ts
@@ -0,0 +1,62 @@
+let score: number | string | boolean = 33; //avoid any, rather put unions in types
+
+score = "55";
+score = true;
+
+//Unions for custom data types
+type User = {
+  name: string;
+  id: string;
+};
+
+type Admin = {
+  username: string;
+  id: string;
+};
+
+let Rajneesh: User | Admin = {
+  name: "Rajneesh",
+  id: "334",
+};
+
+Rajneesh = {
+  username: "Rajneesh",
+  id: "334",
+};
+
+// function getDBID(id: number | string) {
+//   //making some API calls
+//   console.log(`db id is ${id}`);
+// }
+
+// getDBID("3");
+// getDBID(3);
+
+//Let's do some manipulations in that function
+function getDBID(id: number | string) {
+  //making some API calls
+  //   id.toLowerCase() //gives error as id could be number too, how to overcome that?
+  //like this :-
+  if (typeof id === "string") {
+    id.toLowerCase(); // works now
+  } else {
+    //or write else if(typeof id ==="number")
+    id.toFixed(); //a property that can only be used on a number
+  }
+}
+
+getDBID("3");
+getDBID(3);
+
+//arrays
+
+// const data: number[] = [1,2,3,"4"]; //problematic obviosuly, strings aren't allowed
+const data: (string | number | boolean)[] = ["1", 2, 3, true];
+// const data1: string[] | number[] = ["1", 2, 3]; //gives error as that type means either all numbers array or all string array, for that we have to do union first between string and number then put the array sign
+
+// let pi : 3.14 = 3.14; //literal assignment
+// pi = 3.14159; //gives error
+//useful case for literal assignment -> aeroplane seat allotment
+let seatAllotment: "aisle" | "middle" | "window";
+//  seatAllotment = "crew"; //->gives error
+seatAllotment = "middle"; //-> works fine
---
+++ variablesInTS/variables.ts
@@ -0,0 +1,36 @@
+const name: string = "Rajneesh";
+
+let num: number = 21;
+// num = "Rajneesh"; //not allowd, gives error
+let bool: boolean = true;
+//automatic type inference
+//if the variable is declared and given a value at the time
+//of declaration then for simple variables and arrays - type is inferred automatically
+
+let number = 21; //-> type inferred, because declared and value is given at thge time of declaration
+let myName = "Rajneesh"; //-> type inferred
+// number = "R"; //not allowed as automatic type inferrence know thats number is of number type
+
+//for complex variables like complex objects or arrays of objects type may not be inferred automtcially
+let person = {
+  name: "Rajneesh",
+  age: 21,
+};
+
+// person.sex = "male"; //not allowed as automatic type inferrence knows that person has only two properties,i.e., name and age, hence the third property couldn't be added/edited
+
+//use automatic type inferrence whenever possible and avoid the 'any' type
+
+let some; //'any' type as no value is given initially hence automatic type inferrence assigns it the type 'any'
+some = 21; //allowed as type is any
+some = "Rajneesh"; //allowed as type is any
+some = true; //allowed as type is any
+
+//one can assign 'any' type explicitly too in order to avoid defining accurate types or when the type is ambiguous, though do avoid it
+let checkVar: any = 21;
+checkVar = "hey";
+checkVar = true;
+//any type means type could be anything, hence should be avoided as the whole concept of TS is to make JS predictable in case of types atleast. Also use union types(in coming lessons) instead.
+
+export {}; //to remove that error when it says block scoped
+//variable can't be reassigned -> don't worry too much about it
