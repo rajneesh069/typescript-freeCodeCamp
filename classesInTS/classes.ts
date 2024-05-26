3fc83093098ede9c3ee56eedad53f83e56ca8452
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-05-26 11:52:08 +0530

    classes in TS part 1 complete

---
+++ classesInTS/classes.ts
@@ -0,0 +1,13 @@
+class User {
+  email: string;
+  name: string;
+  readonly city: string = "Gonda";
+  address: string;
+  constructor(email: string, name: string) {
+    this.email = email; //this.email refers to the email defined above in the class
+    this.name = name; //this.name refers to the email defined above in the class
+  }
+}
+
+const rajneesh = new User("rajneesh@gmail.com", "Rajneesh");
+rajneesh.address = "Gayatri Puram Colony";
